e6926d3795371fbbea820e85a5c3501ac4eb3e71
施嘉伟 <shijiawei@aodiansoft.com>
2021-04-09 17:06:51 +0800

    基础可配置选项完成

--- index.js
+++ index.js
@@ -3,45 +3,89 @@ import tippy from 'tippy.js'
 import 'tippy.js/dist/tippy.css'
 import 'tippy.js/themes/light.css'
 import './public.css'
+import ClipboardJS from 'clipboard'
 
 class BtnQRCode {
-  constructor(trigger, options) {
-    this.trigger = trigger
+  constructor(options) {
     this.options = options
   }
 
   init() {
-    const { url: imgUrl, text: openText = '', btnText = '预览' } = this.options
-    const trigger = document.getElementById(this.trigger)
-    let openTextDom = ''
+    const {
+      trigger,
+      btnText = '预览',
+      isShowInput = false,
+      openUrlText = '',
+      openUrlType = 'default',
+      copyUrlText = '',
+      copyUrlType = 'default',
+      urlTextPosTB = 'bottom',
+      urlTextPosLR = 'between',
+      noticeText = '',
+    } = this.options
 
-    if (Array.isArray(openText) && openText.length !== 0) {
-      let textTemp = ''
-      openText.forEach((item) => {
-        textTemp =
-          textTemp + `<a href=${item[1]} target="_blank">${item[0]}</a>`
-      })
-      openTextDom = `<div id="openTextDom">${textTemp}</div>`
-    }
+    Array.from(document.querySelectorAll(trigger)).forEach((item, index) => {
+      const url = item.dataset.btnqrcode
 
-    // translate
-    QRCode.toDataURL(imgUrl)
-      .then((url) => {
-        trigger.innerHTML = `<button id="myButton">${btnText}</button>`
+      // 初始化文字链接、输入框链接domx
+      let urlTextDom = ''
+      let inputDom = ''
+      let noticeDom = ''
+      let imgDom = ''
 
-        tippy('#myButton', {
-          theme: 'light',
-          allowHTML: true,
-          duration: [500, 500],
-          // Determines if the tippy has interactive content inside of it, so that it can be hovered over and clicked inside without hiding.
-          interactive: true,
-          content: `<img src=${url} />` + openTextDom,
-          // content: `<img src=${url} />`
+      // 可配置显示输入框
+      if (isShowInput) {
+        inputDom = `<input class="btnqrcode-input-dom" readonly="readonly" value=${url} />`
+      }
+
+      if (noticeText) {
+        noticeDom = `<div class="btnqrcode-notice-dom"><span>${noticeText}</span></div>`
+      }
+
+      // 可配置文字链接、复制操作
+      if (openUrlText || copyUrlText) {
+        let textTemp = ''
+        if (openUrlText) {
+          textTemp = `<div class="btnqrcode-open-url-text btnqrcode-open-url-text-${openUrlType}"><a href=${url} target="_blank">${openUrlText}</a></div>`
+        }
+        if (copyUrlText) {
+          textTemp =
+            textTemp +
+            `<div class="btnqrcode-copy-url-text btnqrcode-copy-url-text-${copyUrlType}" data-clipboard-text="${url}"><a href="javascript:;">${copyUrlText}</a></div>`
+          new ClipboardJS('.btnqrcode-copy-url-text')
+        }
+
+        urlTextDom = `<div class="btnqrcode-url-text-dom btnqrcode-url-text-dom-${urlTextPosLR}">${textTemp}</div>`
+      }
+
+      // 链接转二维码
+      QRCode.toDataURL(url)
+        .then((url) => {
+          item.innerHTML = `<button class="btnqrcode-button-${index}"><span>${btnText}</span></button>`
+
+          imgDom = `<img class="btnqrcode-qrcode" src=${url} />`
+
+          tippy(`.btnqrcode-button-${index}`, {
+            theme: 'light',
+            allowHTML: true,
+            duration: [500, 500],
+
+            // 调试专用
+            // hideOnClick: false,
+            // trigger: 'click',
+
+            // 气泡框内mouseenter不消失
+            interactive: true,
+            content:
+              urlTextPosTB === 'bottom'
+                ? inputDom + imgDom + noticeDom + urlTextDom
+                : inputDom + urlTextDom + imgDom + noticeDom,
+          })
         })
-      })
-      .catch((err) => {
-        console.error(err)
-      })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
   }
 }
 
